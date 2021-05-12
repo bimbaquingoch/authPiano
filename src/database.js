6d99de8aea0c4b5518f11bae6f05d257a2225b85
Kevin Changoluisa <46755578+KevinChangoluisa@users.noreply.github.com>
2021-05-12 12:08:56 -0500

    Conexion mongo atlas
    conexion al servidor Atlas MongoDB

--- src/database.js
+++ src/database.js
@@ -4,11 +4,12 @@ const mongoose = require("mongoose");
 // llamamos a las variables gloables del modulo
 // process.env, son variables de entornor que vienen
 // ya instaladas en NODEJS
-const { PIANO_MONGODB_HOST, PIANO_MONGODB_DATABASE } = process.env;
+const {PIANO_MONGODB_DATABASE,PIANO_MONGODB_PASSWORD } = process.env;
 
 // le decimos desde donde queremos que se ejecute
-const mongodbURL = `mongodb://${PIANO_MONGODB_HOST}/${PIANO_MONGODB_DATABASE}`;
+//const mongodbURL = `mongodb://${PIANO_MONGODB_HOST}/${PIANO_MONGODB_DATABASE}`;
 
+const mongodbURL=`mongodb+srv://K3vin:${PIANO_MONGODB_PASSWORD}@cluster0.t5cs5.mongodb.net/${PIANO_MONGODB_DATABASE}?retryWrites=true&w=majority`
 mongoose
   .connect(mongodbURL, {
     useUnifiedTopology: true,
